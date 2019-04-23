467437192e642fecc74ed7f8bde3db806f88e583
Alfred Obialo <alfredcsdinc@gmail.com>
2019-04-23 14:22:44 +0100

    Create a new Project [Costing App] and add the UserIdentity Class template

---
+++ ItemCostPlatform/Auth/UserCompany.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemCostingPlatform.Auth
+{
+    public class UserCompany
+    {
+        public Address ContactAddress { get; set; }
+        public string Name { get; set; }
+        public string PhoneNo { get; set; }
+        public string  WebsiteUrl { get; set; }
+
+
+    }
+}
---
+++ ItemCostPlatform/Auth/UserIdentity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ItemCostingPlatform.Auth
+{
+    public class UserIdentity
+    {
+        public string  Id { get; set; }
+        public string FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string  EmailAddress { get; set; }
+        //public Address ContactAddress { get; set; } Not Relevant we can get this from user's company info
+        public UserCompany CompanyInfo { get; internal set; }
+
+        public UserIdentity()
+        {
+            // Instantiate  Company info
+            CompanyInfo = new UserCompany();
+        }
+
+        public UserIdentity(string id, string firstName, string lastName) :this()
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id ?? Guid.NewGuid().ToString();
+        }
+
+
+        public string FullName => $"{FirstName ?? "" } {LastName ?? ""}"; 
+    }
+}
